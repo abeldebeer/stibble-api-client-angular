3c07cc99fe2248e4d337082f346405f48d126460
Abel de Beer <abel@cookingfox.nl>
2017-04-07 22:36:54 +0200

    Simplify lib index; Increase version

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,14 +1,14 @@
-export { StibbleApiClientModule } from "./stibble-api-client.module";
+export * from "./stibble-api-client.module";
 
-export { ClientConfig } from "./client/client-config.service";
+export * from "./client/client-config.service";
 export * from "./client/client-constants";
 
-export { AbstractTokenStorage } from "./token/abstract-token-storage";
-export { InMemoryTokenStorage } from "./token/in-memory-token-storage";
-export { LocalStorageTokenStorage } from "./token/local-storage-token-storage";
-export { TokenGateway } from "./token/token-gateway.service";
-export { TokenPayload } from "./token/token-payload";
-export { TokenService } from "./token/token-service.service";
-export { TokenStorageProvider } from "./token/token-storage-provider.service";
-export { TokenStorage } from "./token/token-storage";
-export { Token } from "./token/token";
+export * from "./token/abstract-token-storage";
+export * from "./token/in-memory-token-storage";
+export * from "./token/local-storage-token-storage";
+export * from "./token/token-gateway.service";
+export * from "./token/token-payload";
+export * from "./token/token-service.service";
+export * from "./token/token-storage-provider.service";
+export * from "./token/token-storage";
+export * from "./token/token";
