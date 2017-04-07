26e6eed10635a518bb6a94526bf52329edd416b0
Abel de Beer <abel@cookingfox.nl>
2017-04-07 11:02:16 +0200

    Update README

--- src/client/client-constants.ts
+++ src/client/client-constants.ts
@@ -2,7 +2,7 @@
 // DEFAULTS
 //-------------------------------------------------------------------------------------------------
 
-export const DEFAULT_BASE_URL = 'http://api.stibble.com';
+export const DEFAULT_BASE_URL = 'https://api.stibble.com';
 export const DEFAULT_LOCAL_STORAGE_TOKEN_KEY = 'stibble_api_client_angular_token';
 
 //-------------------------------------------------------------------------------------------------
